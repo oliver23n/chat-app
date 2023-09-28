5d16a1830613991e08d839b7c365cb746834fd9a
oliver23n <oliver.nasevski@gmail.com>
2023-09-28 14:06:30 -0400

    rendering Mymessage (the img if its an image, text if it is text

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -8,7 +8,7 @@ const {chats, activeChat, userName, messages} = props;
 const chat = chats && chats[activeChat];
 
 const renderMessages = ()=>{
-    const keys = Objet.keys(messages);
+    const keys = Object.keys(messages);
     return keys.map((key, index) =>{
         const message = messages[key];
         const lastMessageKey = index == 0 ? null : keys[index-1];
@@ -18,7 +18,7 @@ const renderMessages = ()=>{
             <div key={`msg_${index}`} style = {{width:'100%'}}>
                 <div className="message-block">
                     {
-                        isMyMessage ? <MyMessage /> : <OtherMessage/>
+                        isMyMessage ? <MyMessage  message = {message}/> : <OtherMessage message = {message}  lastMessage={messages[lastMessageKey]}  />
                     }
                 </div>
                 <div className="read-receipts" style={{marginRight: isMyMessage ? '18px' : '0px', marginLeft: isMyMessage ? '0px' : '68px'}}>
--- src/components/MyMessage.jsx
+++ src/components/MyMessage.jsx
@@ -1,8 +1,18 @@
-const MyMessage = () => {
-
+const MyMessage = ({message}) => {
+//if the message is a message or an image
+if (message?.attachments?.length > 0){
     return (
-        <div>
-            my message
+        <img
+            src={message.attachments[0].file}
+            alt="message-img"
+            className="message-image"
+            style={{float: 'right'}}
+        />
+    )
+}
+    return (
+        <div className="message" style={{float:'right', marginRight:'18px', color:'white', backgroundColor: 'blue'}}>
+            {message.text}
         </div>
     )
 }
