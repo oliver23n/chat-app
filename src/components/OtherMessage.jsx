a390dac3834fe382cf981b8050568ca602772087
oliver23n <oliver.nasevski@gmail.com>
2023-09-28 14:19:02 -0400

    othermessage component rendering

--- src/components/OtherMessage.jsx
+++ src/components/OtherMessage.jsx
@@ -1,8 +1,31 @@
-const OtherMessage = () => {
+const OtherMessage = ({ lastMessage, message }) => {
+
+    const isFirstMessageByUser = !lastMessage || lastMessage.sender.username != message.sender.username;
+
 
     return (
-        <div>
-            Other message
+        <div className="message-row">
+
+            {isFirstMessageByUser && (
+                <div className="message-avatar"
+                    style={{ backgroundImage: `url(${message?.sender?.avatar})` }}
+                />
+            )}
+
+            {message?.attachments?.length > 0
+                ? (
+                    <img
+                        src={message.attachments[0].file}
+                        alt="message-img"
+                        className="message-image"
+                        style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
+                    />
+                ) : (
+                    <div className="message" style={{ float: 'left', backgroundColor: 'gray', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
+                        {message.text}
+                    </div>
+                )
+            }
         </div>
     )
 }
